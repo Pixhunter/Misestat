4f70a90860ecdf6ffaebb8c825fc56a6aaf18ae3
Pix.hunter <88940110+Pixhunter@users.noreply.github.com>
2021-10-31 17:02:59 +0300

    Update Main_mouse.py

--- Main_mouse.py
+++ Main_mouse.py
@@ -56,29 +56,29 @@ def schet_run( ):
 
             event = events.get(1.0)
             if event is None:
-            
+            # lets count time procrastination
                 time_not_work=time_not_work+1
                 window.update()
                 if (time_not_work<60):
-                    lbl_value_t['text'] = str(time_not_work) + ' сек'
+                    lbl_value_t['text'] = str(time_not_work) + ' sec'
                 elif (time_not_work<3600):
-                    lbl_value_t['text'] = str(int(time_not_work/60)) + ' мин ' + str(time_not_work - (int(time_not_work/60))*60) + ' сек'
+                    lbl_value_t['text'] = str(int(time_not_work/60)) + ' min ' + str(time_not_work - (int(time_not_work/60))*60) + ' sec'
                 elif (time_not_work<86400):
-                    lbl_value_t['text'] = str(int(time_not_work/3600)) + ' часов ' + str((int(time_not_work/60))-(int(time_not_work/3600))*60) + ' мин ' + str(time_not_work - (int(time_not_work/60))*60) + ' сек'
+                    lbl_value_t['text'] = str(int(time_not_work/3600)) + ' hours ' + str((int(time_not_work/60))-(int(time_not_work/3600))*60) + ' min ' + str(time_not_work - (int(time_not_work/60))*60) + ' sec'
                 else:
-                    lbl_value_t['text'] = str(int(time_not_work/86400)) + ' дней ' +str(-(int(time_not_work/86400)*24) + int(time_not_work/3600)) + ' часов ' + str((int(time_not_work/60))-(int(time_not_work/3600))*60) + ' мин ' + str(time_not_work - (int(time_not_work/60))*60) + ' сек'
+                    lbl_value_t['text'] = str(int(time_not_work/86400)) + ' days ' +str(-(int(time_not_work/86400)*24) + int(time_not_work/3600)) + ' hours ' + str((int(time_not_work/60))-(int(time_not_work/3600))*60) + ' min ' + str(time_not_work - (int(time_not_work/60))*60) + ' sec'
 
                 
-                if (time_not_workm <= time_not_work):
+                if (time_not_workm <= time_not_work): #time p record
                     time_not_workm=time_not_work
                     if (time_not_workm<60):
-                        lbl_value_tm['text'] = str(time_not_workm) + ' сек'
+                        lbl_value_tm['text'] = str(time_not_workm) + ' sec'
                     elif (time_not_workm<3600):
-                        lbl_value_tm['text'] = str(int(time_not_workm/60)) + ' мин ' + str(time_not_workm - (int(time_not_workm/60))*60) + ' сек'
+                        lbl_value_tm['text'] = str(int(time_not_workm/60)) + ' min ' + str(time_not_workm - (int(time_not_workm/60))*60) + ' sec'
                     elif (time_not_workm<86400):
-                        lbl_value_tm['text'] = str(int(time_not_workm/3600)) + ' часов ' + str((int(time_not_workm/60))-(int(time_not_workm/3600))*60) + ' мин ' + str(time_not_workm - (int(time_not_workm/60))*60) + ' сек'
+                        lbl_value_tm['text'] = str(int(time_not_workm/3600)) + ' hours ' + str((int(time_not_workm/60))-(int(time_not_workm/3600))*60) + ' min ' + str(time_not_workm - (int(time_not_workm/60))*60) + ' sec'
                     else:
-                        lbl_value_tm['text'] = str(int(time_not_work/86400)) + ' дней ' +str(-(int(time_not_work/86400)*24) + int(time_not_work/3600)) + ' часов ' + str((int(time_not_work/60))-(int(time_not_work/3600))*60) + ' мин ' + str(time_not_work - (int(time_not_work/60))*60) + ' сек'
+                        lbl_value_tm['text'] = str(int(time_not_work/86400)) + ' days ' +str(-(int(time_not_work/86400)*24) + int(time_not_work/3600)) + ' hours ' + str((int(time_not_work/60))-(int(time_not_work/3600))*60) + ' min ' + str(time_not_work - (int(time_not_work/60))*60) + ' sec'
                 window.update()
             else:
                 time_not_work=0
@@ -104,9 +104,10 @@ def schet_run( ):
                     x0=0
                     y0=0
                     mem=0
-                    
-                    lbl_value['text'] = '0 см'
-                    lbl_value_s['text'] = '0 см'
+
+                    #start values
+                    lbl_value['text'] = '0 cm'
+                    lbl_value_s['text'] = '0 cm'
                     lbl_value_c['text'] = '0'
                     lbl_value_cl['text'] = '0'
                     lbl_value_cr['text'] = '0'
@@ -116,12 +117,8 @@ def schet_run( ):
                     il=6
                    
                 s= format(event)
-
-
-
                 if 'Move' in s:
-
-                     
+                #count moving distance
                     s1=s.find(',')
                     
                     M_x=int(s[7:s1:])
@@ -130,10 +127,6 @@ def schet_run( ):
                         mem=0
                     else:
                         mem=round(pow((pow((M_x-x0),2) + pow((M_y-y0),2)).real,0.5),4)
-                        #print ("pow=------  ", (pow((M_x-x0),2) + pow((M_y-y0),2)))
-                        #print ("mem=------  ", mem)
-                    
-#---if u reed it u must know that u suck my dick bro :P
                      
                     if (M_s_run==0):
                         M_s_run=(mem)*koeff
@@ -146,35 +139,34 @@ def schet_run( ):
                             M_s_run=M_s_run + (mem)*koeff/100000
                             
                             
-                    #print("E----",M_s_run)
                     if ((M_s_run < 100) & (k==0)):
                         window.update()
-                        lbl_value['text'] = str(round(M_s_run,2)) + ' см'
+                        lbl_value['text'] = str(round(M_s_run,2)) + ' cm'
                         window.update()
                         
 
                     elif (((M_s_run >= 100) & (M_s_run < 1000)) & (k==0)):     
                         window.update()
                         M_s_run=M_s_run/100
-                        lbl_value['text'] = str(round(M_s_run,2)) + ' м'
+                        lbl_value['text'] = str(round(M_s_run,2)) + ' m'
                         window.update()
                         k=1
                         
                     elif ((M_s_run < 1000) & (k==1)):
                         window.update()
-                        lbl_value['text'] = str(round(M_s_run,2)) + ' м'
+                        lbl_value['text'] = str(round(M_s_run,2)) + ' m'
                         window.update()
 
                     elif (((M_s_run >= 1000)) & (k==1)):     
                         window.update()
                         M_s_run=M_s_run/1000
-                        lbl_value['text'] = str(round(M_s_run,2)) + ' км'
+                        lbl_value['text'] = str(round(M_s_run,2)) + ' km'
                         window.update()
                         k=2
                         
                     elif ( (k==2)):
                         window.update()
-                        lbl_value['text'] = str(round(M_s_run,2)) + ' км'
+                        lbl_value['text'] = str(round(M_s_run,2)) + ' km'
                         window.update()
 
                         
@@ -182,7 +174,8 @@ def schet_run( ):
                     y0=M_y
                     
                 if 'Click' in s:
-                     
+                #count clicks
+                    
                     s1=s.find('.')
                     C_click=s[s1+1:(len(s)-16):]
  
@@ -197,46 +190,44 @@ def schet_run( ):
                     lbl_value_cl['text'] = str(int(count_click_l/2)) 
                     lbl_value_cr['text'] = str(int(count_click_r/2)) 
                     window.update()
-#---if u reed it u must know that u suck my dick bro :P 
+
                     
                 if 'Scroll' in s:
-
+                #count scrolling distance
                     scroll_roll=(scroll_roll+1)
-                    #lbl_value_s['text'] = str((scroll_roll*0.5)) + ' cm'
+
                     if ((scroll_roll*0.5 < 100) & (ks==0)):
                         window.update()
-                        lbl_value_s['text'] = str(round(scroll_roll*0.5,2)) + ' cм'
+                        lbl_value_s['text'] = str(round(scroll_roll*0.5,2)) + ' cm'
                         window.update()
-                        #print("----55")
+                        
 
                     elif (((scroll_roll*0.5 >= 100) & (M_s_run < 1000)) & (ks==0)):     
                         window.update()
                         scroll_roll=scroll_roll/100
-                        lbl_value_s['text'] = str(round((scroll_roll)*0.5,2)) + ' м'
+                        lbl_value_s['text'] = str(round((scroll_roll)*0.5,2)) + ' m'
                         window.update()
                         ks=1
                         
                     elif ((scroll_roll*0.5 < 1000) & (ks==1)):
                         window.update()
-                        lbl_value_s['text'] = str(round(scroll_roll*0.005,2)) + ' м'
+                        lbl_value_s['text'] = str(round(scroll_roll*0.005,2)) + ' m'
                         window.update()
 
                     elif (((scroll_roll*0.5 >= 1000)) & (ks==1)):     
                         window.update()
                         scroll_roll=scroll_roll/1000
-                        lbl_value_s['text'] = str(round(scroll_roll,2)) + ' км'
+                        lbl_value_s['text'] = str(round(scroll_roll,2)) + ' km'
                         window.update()
                         ks=2
                         
                     elif ( (ks==2)):
                         window.update()
-                        lbl_value_s['text'] = str(round(scroll_roll*0.5,2)) + ' км'
+                        lbl_value_s['text'] = str(round(scroll_roll*0.5,2)) + ' km'
                         window.update()
                         
                     window.update()
 
-#---if u reed it u must know that u suck my dick bro :P
-                    # ---------PIX.HUNTER-------2021
    
 def stop():
     global doTick
@@ -276,8 +267,8 @@ def obnulenie():
     
 def user_name_re():
     window.update()
-    lbl_0['text']='\nТвоя статистика мыши, \n' + str(os.environ.get( "USERNAME" )) + '\n'
-#---if u reed it u must know that u suck my dick bro :P    
+    lbl_0['text']='\nYour mouse statistics, \n' + str(os.environ.get( "USERNAME" )) + '\n'
+  
 #-------------------------------------------------------------------------------------------------------
 #          ENTER
 #-------------------------------------------------------------------------------------------------------
@@ -303,12 +294,8 @@ window.title("Мышовая статистика")
 window.geometry('420x560')
 window.configure(background="black")
 window.resizable(False, False)
-#window.iconbitmap('temp\icoco.ico')
 
-os.environ.get( "USERNAME" )
-
-#print (os.environ.get( "USERNAME" ))
-#---if u reed it u must know that u suck my dick bro :P
+os.environ.get( "USERNAME" ) #i need your name ^___^
 
 lbl_0 = tk.Label(master=window, text="\n0",background="black", fg='#ff086b', font=courier_18)
 lbl_0.grid(row=0, column=1, columnspan=6)
@@ -316,29 +303,19 @@ user_name_re()
 
 lbl_1_0 = tk.Label(master=window, background="black",text="       ").grid(row=0, column=0, sticky="W", padx=0, rowspan=25)
 
-lbl_1_0 = tk.Label(master=window,  background="black",text="Расстояния:", fg='lime', font=courier_10).grid(row=1, column=1, sticky="W", padx=0, columnspan=3)
-lbl_2_0 = tk.Label(master=window, background="black",text="    - движение курсором:", fg='white', font=courier_16).grid(row=2, column=1, sticky="W", padx=0, columnspan=3)
-lbl_3_0 = tk.Label(master=window, background="black",text="    - прокрутка колесика:\n", fg='white', font=courier_16).grid(row=3, column=1, sticky="W", padx=0, columnspan=3)
-lbl_4_0 = tk.Label(master=window, background="black",text="Шелчков мыши:", fg='#ffb121', font=courier_10).grid(row=4, column=1, sticky="W", padx=0, columnspan=3)
-lbl_4_0 = tk.Label(master=window, background="black",text="    - левых:", fg='white', font=courier_16).grid(row=5, column=1, sticky="W", padx=0, columnspan=3)
-lbl_4_0 = tk.Label(master=window, background="black",text="    - правых:", fg='white', font=courier_16).grid(row=6, column=1, sticky="W", padx=0, columnspan=3)
-lbl_4_0 = tk.Label(master=window, background="black",text="    - всего:\n", fg='#f74040', font=courier_16).grid(row=7, column=1, sticky="W", padx=0, columnspan=3)
-lbl_4_0 = tk.Label(master=window, background="black",text="Время бездействия:", fg='deepskyblue', font=courier_10).grid(row=9, column=1, sticky="W", padx=0, columnspan=3)
-lbl_4_0 = tk.Label(master=window, background="black",text="Рекорд бездействия:", fg='magenta', font=courier_10).grid(row=10, column=1, sticky="W", pady=0, columnspan=3)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░░░▐▀▄──────▄▀▌───▄▄▄▄▄▄▄").grid(row=11, column=1, pady=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░░░▌▒▒▀▄▄▄▄▀▒▒▐▄▀▀▒██▒██▒▀▀▄").grid(row=12, column=1, pady=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄").grid(row=13, column=1, padx=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░░▌▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄").grid(row=14, column=1, padx=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░▀█▒▒█▌▒▒█▒▒▐█▒▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌").grid(row=15, column=1, padx=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░▀▌▒▒▒▒▒▀▒▀▒▒▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐ ▄▄").grid(row=16, column=1, padx=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄█▒█").grid(row=17, column=1, padx=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▀").grid(row=18, column=1, padx=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░░▐▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▌").grid(row=19, column=1, padx=0, sticky="W", columnspan=3,)
-#lbl_4_0 = tk.Label(master=window, background="darkmagenta",text="░░░░░░░▀▄▄▀▀▀▀▄▄▀▀▀▀▀▀▄▄▀▀▀▀▀▀▄▄").grid(row=20, column=1, padx=0, sticky="W", columnspan=3,)
+lbl_1_0 = tk.Label(master=window,  background="black",text="Distances:", fg='lime', font=courier_10).grid(row=1, column=1, sticky="W", padx=0, columnspan=3)
+lbl_2_0 = tk.Label(master=window, background="black",text="    - cursor movement:", fg='white', font=courier_16).grid(row=2, column=1, sticky="W", padx=0, columnspan=3)
+lbl_3_0 = tk.Label(master=window, background="black",text="    - scroll wheel:\n", fg='white', font=courier_16).grid(row=3, column=1, sticky="W", padx=0, columnspan=3)
+lbl_4_0 = tk.Label(master=window, background="black",text="Mouse clicks:", fg='#ffb121', font=courier_10).grid(row=4, column=1, sticky="W", padx=0, columnspan=3)
+lbl_4_0 = tk.Label(master=window, background="black",text="    - left:", fg='white', font=courier_16).grid(row=5, column=1, sticky="W", padx=0, columnspan=3)
+lbl_4_0 = tk.Label(master=window, background="black",text="    - right:", fg='white', font=courier_16).grid(row=6, column=1, sticky="W", padx=0, columnspan=3)
+lbl_4_0 = tk.Label(master=window, background="black",text="    - all:\n", fg='#f74040', font=courier_16).grid(row=7, column=1, sticky="W", padx=0, columnspan=3)
+lbl_4_0 = tk.Label(master=window, background="black",text="Idle time:", fg='deepskyblue', font=courier_10).grid(row=9, column=1, sticky="W", padx=0, columnspan=3)
+lbl_4_0 = tk.Label(master=window, background="black",text="Best idle time:", fg='magenta', font=courier_10).grid(row=10, column=1, sticky="W", pady=0, columnspan=3)
+
 lbl_4_0 = tk.Label(master=window, background="black", fg='white', font=courier_16,text="\n\n------------------------PIX.HUNTER-------------------------\n\n").grid(row=21, column=1, padx=0, sticky="W", columnspan=5)
-                                      
-# ---------PIX.HUNTER-------2021  
-#---if u reed it u must know that u suck my dick bro :P
+
+
 
 lbl_value = tk.Label(master=window, text="0" , background="black", fg='white', font=courier_16)
 lbl_value.grid(row=2, column=3, columnspan=3, sticky="E")
@@ -356,11 +333,24 @@ lbl_value_t.grid(row=9, column=3, columnspan=3, sticky="E")
 lbl_value_tm= tk.Label(master=window, text="0" , background="black", fg='magenta', font=courier_16)
 lbl_value_tm.grid(row=10, column=3, columnspan=3, sticky="E")
 
-# ---------PIX.HUNTER-------2021
-btn2 = Button(text="Старт", command=start, bg='#00ff00', fg='black', activebackground="magenta", font=courier_1, width=5).grid(row=25, column=2, sticky="nsew", padx=5, pady=5)
-btn3 = Button(text="Обнулить", command=obnulenie,  bg='#ff6200', fg='yellow', activebackground="yellow", font=courier_1, width=13).grid(row=25, column=3, sticky="nsew", padx=5, pady=5, columnspan=2)
 
-btn4 = Button(text="Стоп", command=stop, width=5, height=1,bg='#cc0000', fg='white', activebackground="cyan", font=courier_1).grid(row=25, column=5, sticky="nsew", padx=5, pady=5)
+btn2 = Button(text="Start", command=start, bg='#00ff00', fg='black', activebackground="magenta", font=courier_1, width=5).grid(row=25, column=2, sticky="nsew", padx=5, pady=5)
+btn3 = Button(text="Revoke", command=obnulenie,  bg='#ff6200', fg='yellow', activebackground="yellow", font=courier_1, width=13).grid(row=25, column=3, sticky="nsew", padx=5, pady=5, columnspan=2)
 
-# ---------PIX.HUNTER-------2021
+btn4 = Button(text="Stop", command=stop, width=5, height=1,bg='#cc0000', fg='white', activebackground="cyan", font=courier_1).grid(row=25, column=5, sticky="nsew", padx=5, pady=5)
+
+
 window.mainloop()
+
+
+#░░░░░░░▐▀▄──────▄▀▌───▄▄▄▄▄▄▄
+#░░░░░░░▌▒▒▀▄▄▄▄▀▒▒▐▄▀▀▒██▒██▒▀▀▄
+#░░░░░░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄
+#░░░░░░▌▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄
+#░░░░░▀█▒▒█▌▒▒█▒▒▐█▒▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌
+#░░░░░▀▌▒▒▒▒▒▀▒▀▒▒▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐ ▄▄
+#░░░░░░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄█▒█
+#░░░░░░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▀
+#░░░░░░▐▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▌
+#░░░░░░░▀▄▄▀▀▀▀▄▄▀▀▀▀▀▀▄▄▀▀▀▀▀▀▄▄
+# ---------PIX.HUNTER-------2021
